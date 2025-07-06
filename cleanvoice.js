196606938632ac9c314ca3296eade0bd0bcf9cac
Alekskorg <akozevnikov635@gmail.com>
2025-07-06 20:12:02 +0300

    Update cleanvoice.js

--- cleanvoice.js
+++ cleanvoice.js
@@ -1,14 +1,11 @@
-<!DOCTYPE html><html lang="ru"><head>
-<meta charset="UTF-8">
-<title>CleanVoice</title>
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<!-- React и Babel напрямую из CDN -->
-<script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
-<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
-<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
-</head><body>
-  <div id="root"></div>
+function CleanVoiceApp() {
+  return (
+    <div>
+      <h1>CleanVoice работает!</h1>
+      <p>Добро пожаловать в твой онлайн-сервис.</p>
+    </div>
+  );
+}
 
-  <!-- Подключаем наш упрощённый скрипт -->
-  <script type="text/babel" src="cleanvoice.js"></script>
-</body></html>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<CleanVoiceApp />);
